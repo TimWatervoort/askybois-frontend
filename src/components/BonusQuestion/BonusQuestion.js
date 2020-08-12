394a6dc4fae45a5f9190f7250a60df94461803e2
timwatervoort <twwatervoort@gmail.com>
2020-08-12 17:48:09 -0600

    q

--- src/components/BonusQuestion/BonusQuestion.js
+++ src/components/BonusQuestion/BonusQuestion.js
@@ -4,8 +4,8 @@ const BonusQuestion = props => {
   return (
     <div className='row p-3 ask-bq-body'>
         <div className='col'>
-          <h3 className='ask-bq-heading my-5'>BONUS QUESTION 8/5</h3>
-          <p className='ask-bq-question'>What company owned the now-defunct automobile brands Pontiac, Saturn, and Hummer?</p>
+          <h3 className='ask-bq-heading my-5'>BONUS QUESTION 8/12</h3>
+          <p className='ask-bq-question'>What is the most popular girl's name of 2020?</p>
           <p className='ask-bq-points mb-5'>1 POINT</p>
       </div>    
     </div>
