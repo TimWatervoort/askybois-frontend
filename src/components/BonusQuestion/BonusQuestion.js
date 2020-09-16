c9124143eeb13b0a8cfa33c844bd53da61fb4408
timwatervoort <twwatervoort@gmail.com>
2020-09-16 14:32:26 -0600

    updates

--- src/components/BonusQuestion/BonusQuestion.js
+++ src/components/BonusQuestion/BonusQuestion.js
@@ -4,8 +4,8 @@ const BonusQuestion = props => {
   return (
     <div className='row p-3 ask-bq-body'>
         <div className='col'>
-          <h3 className='ask-bq-heading my-5'>BONUS QUESTION 9/9</h3>
-          <p className='ask-bq-question'>What extinct language was spoken by Germanic tribes and is the earliest known Germanic language?</p>
+          <h3 className='ask-bq-heading my-5'>BONUS QUESTION 9/16</h3>
+          <p className='ask-bq-question'>What is the most popular male name of the last 100 years in the US?</p>
           <p className='ask-bq-points mb-5'>1 POINT</p>
       </div>    
     </div>
