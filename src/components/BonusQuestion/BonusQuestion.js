5f49c8967b50f51ebad96ec1c71783425e609430
timwatervoort <twwatervoort@gmail.com>
2020-09-23 15:39:27 -0600

    new bonus q

--- src/components/BonusQuestion/BonusQuestion.js
+++ src/components/BonusQuestion/BonusQuestion.js
@@ -4,8 +4,8 @@ const BonusQuestion = props => {
   return (
     <div className='row p-3 ask-bq-body'>
         <div className='col'>
-          <h3 className='ask-bq-heading my-5'>ATLA BONUS QUESTION</h3>
-          <p className='ask-bq-question'>What is the name of Iroh’s tea shop in Ba Sing Se?</p>
+          <h3 className='ask-bq-heading my-5'>BONUS QUESTION 9/23</h3>
+          <p className='ask-bq-question'>What was the capital of the Republic of Texas before it moved to Austin?</p>
           <p className='ask-bq-points mb-5'>1 POINT</p>
       </div>    
     </div>
