0195c36a34dd811361faa2b1ddd1623f3fbfe21b
timwatervoort <twwatervoort@gmail.com>
2020-10-07 18:25:29 -0600

    update

--- src/components/BonusQuestion/BonusQuestion.js
+++ src/components/BonusQuestion/BonusQuestion.js
@@ -4,8 +4,8 @@ const BonusQuestion = props => {
   return (
     <div className='row p-3 ask-bq-body'>
         <div className='col'>
-          <h3 className='ask-bq-heading my-5'>BONUS QUESTION 9/30</h3>
-          <p className='ask-bq-question'>Who created <i>The Simpsons</i>, <i>Futurama</i>, and <i>Disenchantment?</i></p>
+          <h3 className='ask-bq-heading my-5'>BONUS QUESTION 10/7</h3>
+          <p className='ask-bq-question'>In 2001, the world saw the first country fully legalize same-sex marriage. What country?</p>
           <p className='ask-bq-points mb-5'>1 POINT</p>
       </div>    
     </div>
