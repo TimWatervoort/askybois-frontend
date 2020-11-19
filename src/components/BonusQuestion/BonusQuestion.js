316907dea89145bfbc3319a461b521b26fb134f7
timwatervoort <twwatervoort@gmail.com>
2020-11-18 18:29:23 -0700

    updates

--- src/components/BonusQuestion/BonusQuestion.js
+++ src/components/BonusQuestion/BonusQuestion.js
@@ -4,8 +4,8 @@ const BonusQuestion = props => {
   return (
     <div className='row p-3 ask-bq-body'>
         <div className='col'>
-          <h3 className='ask-bq-heading my-5'>BONUS QUESTION 11/11</h3>
-          <p className='ask-bq-question'>What is the nearest star system to our solar system?</p>
+          <h3 className='ask-bq-heading my-5'>BONUS QUESTION 11/18</h3>
+          <p className='ask-bq-question'>Who composed the music for both The Simpsons Movie and The Dark Knight?</p>
           <p className='ask-bq-points mb-5'>1 POINT</p>
       </div>    
     </div>
