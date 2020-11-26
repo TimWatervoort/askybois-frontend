c8a892e76a5da63c6bf963da72058e385530a5de
timwatervoort <twwatervoort@gmail.com>
2020-11-25 18:08:31 -0700

    update

--- src/components/BonusQuestion/BonusQuestion.js
+++ src/components/BonusQuestion/BonusQuestion.js
@@ -4,8 +4,8 @@ const BonusQuestion = props => {
   return (
     <div className='row p-3 ask-bq-body'>
         <div className='col'>
-          <h3 className='ask-bq-heading my-5'>BONUS QUESTION 11/18</h3>
-          <p className='ask-bq-question'>Who composed the music for both The Simpsons Movie and The Dark Knight?</p>
+          <h3 className='ask-bq-heading my-5'>BONUS QUESTION 11/25</h3>
+          <p className='ask-bq-question'>What is another name for a horn of plenty?</p>
           <p className='ask-bq-points mb-5'>1 POINT</p>
       </div>    
     </div>
