0c74b29dafd733564561d3048f4944597479a5e8
timwatervoort <twwatervoort@gmail.com>
2020-12-02 18:07:12 -0700

    update

--- src/components/BonusQuestion/BonusQuestion.js
+++ src/components/BonusQuestion/BonusQuestion.js
@@ -4,8 +4,8 @@ const BonusQuestion = props => {
   return (
     <div className='row p-3 ask-bq-body'>
         <div className='col'>
-          <h3 className='ask-bq-heading my-5'>BONUS QUESTION 11/25</h3>
-          <p className='ask-bq-question'>What is another name for a horn of plenty?</p>
+          <h3 className='ask-bq-heading my-5'>BONUS QUESTION 12/2</h3>
+          <p className='ask-bq-question'>Hong Kong and what other city are the two Special Administrative Regions of China?</p>
           <p className='ask-bq-points mb-5'>1 POINT</p>
       </div>    
     </div>
