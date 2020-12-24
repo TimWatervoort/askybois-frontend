cc55880e866e9691335451feb8f2ff89175e93e0
timwatervoort <twwatervoort@gmail.com>
2020-12-23 18:04:56 -0700

    new bonus

--- src/components/BonusQuestion/BonusQuestion.js
+++ src/components/BonusQuestion/BonusQuestion.js
@@ -4,8 +4,8 @@ const BonusQuestion = props => {
   return (
     <div className='row p-3 ask-bq-body'>
         <div className='col'>
-          <h3 className='ask-bq-heading my-5'>BONUS QUESTION 12/9</h3>
-          <p className='ask-bq-question'>What internet legend invented animutation, wrote the music for Ultimate Showdown, created Potter Puppet Pals, and now creates song mashups?</p>
+          <h3 className='ask-bq-heading my-5'>BONUS QUESTION 12/23</h3>
+          <p className='ask-bq-question'>What Germanic pagan celebration happens every winter?</p>
           <p className='ask-bq-points mb-5'>1 POINT</p>
       </div>    
     </div>
