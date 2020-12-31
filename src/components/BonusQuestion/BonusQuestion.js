70c505e65adb8eb7c09575f1d0405388d8b4b9ed
timwatervoort <twwatervoort@gmail.com>
2020-12-30 18:18:04 -0700

    update

--- src/components/BonusQuestion/BonusQuestion.js
+++ src/components/BonusQuestion/BonusQuestion.js
@@ -4,8 +4,8 @@ const BonusQuestion = props => {
   return (
     <div className='row p-3 ask-bq-body'>
         <div className='col'>
-          <h3 className='ask-bq-heading my-5'>BONUS QUESTION 12/23</h3>
-          <p className='ask-bq-question'>What Germanic pagan celebration happens every winter?</p>
+          <h3 className='ask-bq-heading my-5'>BONUS QUESTION 12/30</h3>
+          <p className='ask-bq-question'>What 2020 Democratic presidential candidate has President-Elect Biden named as his Secretary of Transportation?</p>
           <p className='ask-bq-points mb-5'>1 POINT</p>
       </div>    
     </div>
