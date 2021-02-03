ad95cdb87061436bf6e5e60798a74ba083461944
timwatervoort <twwatervoort@gmail.com>
2021-02-03 16:16:42 -0700

    loads bonus qs from file

--- src/components/BonusQuestion/BonusQuestion.js
+++ src/components/BonusQuestion/BonusQuestion.js
@@ -1,11 +1,14 @@
 import React from 'react';
+import bonusQuestions from 'util/bonusQuestions';
 
 const BonusQuestion = props => {
+  const latestQuestion = bonusQuestions[bonusQuestions.length - 1];
+
   return (
     <div className='row p-3 ask-bq-body'>
         <div className='col'>
-          <h3 className='ask-bq-heading my-5'>BONUS QUESTION 1/27</h3>
-          <p className='ask-bq-question'>What country has an ATM with instructions in Latin?</p>
+          <h3 className='ask-bq-heading my-5'>BONUS QUESTION { latestQuestion.date }</h3>
+          <p className='ask-bq-question'>{ latestQuestion.content }</p>
           <p className='ask-bq-points mb-5'>1 POINT</p>
       </div>    
     </div>
---
+++ src/util/bonusQuestions.js
@@ -0,0 +1,14 @@
+const bonusQuestions = [
+  {
+    id: 'bq_0',
+    content: 'What country has an ATM with instructions in Latin?',
+    date: '1/27',
+  },
+  {
+    id: 'bq_1',
+    content: 'What\'s the most popular brand of hard seltzer?',
+    date: '2/3',
+  }
+];
+
+export default bonusQuestions;
